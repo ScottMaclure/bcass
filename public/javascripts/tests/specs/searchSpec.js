2b4c85f9568326d7fa4574fd34365a65c7dd2e4d
Scott Maclure <scott@maclure.com.au>
2013-05-24 09:32:59 +1000

    Specs update

--- public/javascripts/tests/specs/searchSpec.js
+++ public/javascripts/tests/specs/searchSpec.js
@@ -1,6 +1,7 @@
 /*global $,describe,it,expect,toBe*/
 /**
  * test suite for search feature.
+ * TODO Sinon
  */
 
 describe("search suite", function () {
@@ -23,17 +24,51 @@ describe("search suite", function () {
 		return out.join('');
 	}
 
+	/**
+	 * All tests will create mock html and init the JS plugin.
+	 */
 	beforeEach(function () {
 		$container = $('<div></div>').append(createHtml()).appendTo('body');
 		$searchElem = $container.find('.m-search');
 		plugin = $searchElem.data('search');
 	});
 
-	it('is setup correctly', function () {
-		expect($searchElem.length).toBe(1);
+	/**
+	 * Cleanup - remove container for the next test.
+	 */
+	afterEach(function () {
+		$container.remove();
 	});
 
-	it('has the plugin bound to the element', function () {
+	describe('when the plugin has initialized', function () {
+
+		it('is setup correctly', function () {
+			expect($searchElem.length).toBe(1);
+		});
+
+		it('has the plugin bound to the element', function () {
+			expect(plugin.constructor.name).toEqual('search');
+		});
+
+		it('does not have the is-searchResultsAvailable state class', function () {
+			expect($searchElem.hasClass('is-searchResultsAvailable')).toBe(false);
+		});
+
+	});
+
+	describe('when a user types in a search term and clicks go', function () {
+
+		it('shows a loading spinner', function () {
+
+		});
+
+		it('makes a GET request to the server', function () {
+
+		});
+
+		it('passes the search term', function () {
+
+		});
 
 	});
 
